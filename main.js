6fcc43fa5be12dd42d4fb6face985c51ba2022a4
Ed <ed@MacBook-Pro-de-Ed.local>
2019-04-24 14:31:43 -0500

    Enlace de bootstrap

---
+++ main.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyC8JOhVQud41B-yLTCNMYLStSOfvh_g6DM",
+    authDomain: "dame-5-719fc.firebaseapp.com",
+    databaseURL: "https://dame-5-719fc.firebaseio.com",
+    projectId: "dame-5-719fc",
+    storageBucket: "dame-5-719fc.appspot.com",
+    messagingSenderId: "620329281209"
+  };
+  firebase.initializeApp(config);
+
+  
