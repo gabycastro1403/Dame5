4d48aa2e2ed4a1ade248712ae5e9d5f6e3bd317e
Ed <ed@MacBook-Pro-de-Ed.local>
2019-04-24 21:43:30 -0500

    Subiendo imagen de usuario y nombre de usuario tomados del correo

--- src/data.js
+++ src/data.js
@@ -9,6 +9,7 @@ var config = {
   };
   firebase.initializeApp(config);
   const auth = firebase.auth();
+  
   //Constantes
   
   if(location.href.match(/(index.html)$/g)){
@@ -22,12 +23,24 @@ const loginGoogle = document.getElementById('login-google');
     firebase.auth().signInWithRedirect(baseProvider)
       .catch(e => console.log(e.message));
   });
-
+  
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
      //que lo lleve a la pagina del menu
      alert('si funciono')
      location.replace('./user_account.html')
+    var user = firebase.auth().currentUser;
+    console.log(user);
+    if(user != null){
+    user.providerData.forEach(profile => {
+    alert('Si fuciono');
+      let foto = profile.photoURL;
+      let name = profile.displayName;
+      localStorage.setItem('foto',foto);
+      localStorage.setItem('name', name);
+      
+    });
+  }
     } else {
         alert('Bienvenido')
       //que lo deje en la pagina del logo 
@@ -35,10 +48,19 @@ const loginGoogle = document.getElementById('login-google');
   })
 }else if(location.href.match(/(account.html)$/g)){
   const logOut= document.getElementById('logout');
+  const nameUser = document.getElementById('name-user');
+  const imageUser = document.getElementById('image-user');
+  const userLocal = localStorage.getItem('foto');
+  const nameLocal = localStorage.getItem('name');
 
+  imageUser.innerHTML = `<img class="user-img" src='${userLocal}'>`;
+  nameUser.innerHTML = nameLocal;
   logOut.addEventListener('click', () => {
     firebase.auth().signOut();
     console.log("Usuario fuera");
     location.replace('./index.html')
   });
+
+
+ 
 }
